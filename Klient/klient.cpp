c83ed6ab8e36938f91ea02b2ef9a478b4278b49f
alicja99 <alicja.mruk99@gmail.com>
2019-10-18 23:06:36 +0200

    Updated Server
    Server is sending the correct answer to the client

--- Klient/klient.cpp
+++ Klient/klient.cpp
@@ -3,19 +3,41 @@
 #define UNICODE
 #endif
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
 
-
+constexpr auto PORT = 8000
 
 #include <iostream>
 #include <stdio.h>
 #include <signal.h>
 #include <Windows.h>
 #include <winsock2.h>
+#include <string>
+#include <conio.h>
+#include <ctime>
 
 #pragma comment (lib, "Ws2_32.lib")
 
 using namespace std;
+
+
+string get_godzina() {
+
+	time_t currentTime;
+	struct tm* localTime;
+
+	time(&currentTime);                   // Get the current time
+	localTime = localtime(&currentTime);  // Convert the current time to the local time
+
+	int hour = localTime->tm_hour;
+	int minute = localTime->tm_min;
+
+	string godzina = "";
+	godzina += to_string(hour)+ ":" + to_string(minute);
+
+	return godzina;
+}
 
 int main()
 {
@@ -23,7 +45,6 @@ int main()
 
 
 	static char IP[] = "127.0.0.1"; //host IP
-	unsigned short PORT = 8000;
 	WORD versionRequested = MAKEWORD(2, 2); //wersja WinSock
 
 	cout << "-----UDP CLIENT----- " << endl;
@@ -35,12 +56,21 @@ int main()
 	SOCKET UDPSocketClient;
 	struct sockaddr_in UDPServer;
 
-	char Buffer[512] = "Wiadomosc do wyslania";  //wiadomosc do przeslania 
+	char Buffer[800] = "Wiadomosc do wyslania";  //wiadomosc do przeslania 
 	int iSendTo;
 	
-	int iBuffferLength = strlen(Buffer) + 1; //dlugosc buffera 
-	int iUDPServerLength = sizeof(UDPServer);
+ 
+	int iUDPServer= sizeof(UDPServer);
 	int iCloseSocket;
+
+	//do odebrania wiadomoœci od serwera
+
+	int iReceiveFrom;
+	char BufferReceive[800]; //wiadomosc doebrana 
+	int iBuffferReceiveLength = strlen(BufferReceive) + 1;
+
+
+	//
 
 	//--------------------------------
 	//inicjalizacja WinSock
@@ -65,6 +95,102 @@ int main()
 	cout << "Utworzenie gniazda pomyslne " << endl;
 	//---------------------------
 	//wysylanie wiadomosic do serwera
+	//oper#mnozenie@stat#NULL@iden#sesja0(numer w tablicy 0)#12:00#3#3#3@
+	//tworzenie komunikatu
+	//praca na tablcy char - Buffer
+	//--tworzenie stringa - komunikatu do tablicy Buffer
+
+	static string operacja = "oper#";
+	static string status = "stat#NULL@"; //klient wysyla zawsze NULL
+	string iden = "iden#";
+	string komunikat_koncowy = " ";
+
+	bool dzielenie = false;
+	char wybor;
+
+	//wstawic while true
+	//
+	//
+	//
+
+	//-- test dla jednej operacji -- 
+	cout << "Jaka operacja " << endl;
+	cout << "-----------------"<<endl;
+	cout << "1. Dodawanie " << endl;
+	cout << "2. Odejmowanie " << endl;
+	cout << "3. Mnozenie " << endl;
+	cout << "4. Dzielenie " << endl;
+	
+
+	wybor = _getch();
+	switch(wybor) {
+	case '1':
+		operacja += "dodawanie@";
+		break;
+	case '2':
+		operacja += "odejmowanie@";
+			break;
+	case '3':
+		operacja += "mnozenie@";
+		break;
+	case '4':
+		operacja += "dzielenie@";
+		dzielenie = true;
+		//--sprawdzenie warunku potem - string iden
+		break;
+	
+		
+	}
+	//1. ---- doklejenie numeru sesji klienta do stringa operacja, wstawic tam gdzie inicjalizacja gniazda, poniewaz klient TYLKO RAZ otrzymuje nr sesji
+
+	
+//
+	//
+	//
+	//
+
+	cout << get_godzina();
+	//2. ---- doklejenie godziny do stringa operacja----
+	iden += get_godzina()+"#";
+
+	//3. ---- doklejenie liczb do stringa operacja-----
+
+	//dodawanie liczb do iden (moze byc poza switch, jedynie co trzeba dodac to sprawdzenie 0 gdy operacja jest dzielenie)
+	//---- w dzieleniu tylko PIERWSZA liczba moze byc 0, reszta musi byc rozna od 0----
+	//wpisanie 3 liczb do dzialania
+	int liczba;
+		for (int i = 0; i < 3;) {
+			//wprowadzanie liczby do zmiennej tymczasowej
+			cin >> liczba;
+			//warunek sprawdzajacy czy dana operacja jest dzieleniem lub, w przypadku dzielenia, sprawdza czy aktualnie ustawiana jest pierwsza liczba (w przypadku dzielenia moze byc to 0)
+			if (i == 0 || !dzielenie) {
+				//wpisanie liczby do operacji iden
+				iden += to_string(liczba);
+				iden += (i != 2) ? "#" : "@";
+				i++;
+			}
+			else {
+				//wykrycie czy dana operacja jest dzieleniem
+				if (dzielenie) {
+					//wykrycie czy zostalo wpisane 0 dla drugiej lub trzeciej liczby, jesli nie, liczba zostanie dopisana, jesli tak, krok dopisania zostanie pominiety
+					if (liczba != 0) {
+						iden += to_string(liczba);
+						iden += (i != 2) ? "#" : "@";
+						i++;
+					}
+				}
+			}
+		}
+
+
+	//----wyslanie pe³nego komunikatu
+	komunikat_koncowy = operacja + status + iden;
+	//--zamiana string na char array : Buffer 
+	
+	strcpy(Buffer, komunikat_koncowy.c_str());
+
+	int iBuffferLength = strlen(Buffer) + 1; //dlugosc buffera
+
 	iSendTo = sendto(
 		UDPSocketClient,
 		Buffer,
@@ -72,13 +198,39 @@ int main()
 		MSG_DONTROUTE,
 		(SOCKADDR*)&UDPServer,
 		sizeof(UDPServer));
-	//---------------------------d
+	//---------------------------
+	
 
 	if (iSendTo == SOCKET_ERROR) {
 		cout << "Blad wysylania wiadomosci o numerze " << WSAGetLastError() << endl;
 	}
 	cout << "Wiadomosc wyslana pomyslne " << endl;
 	//---------------------
+	//odebranie komunikatu od serwera
+
+	iReceiveFrom = recvfrom(
+		UDPSocketClient,
+		BufferReceive,
+		iBuffferReceiveLength,
+		MSG_PEEK,
+		(SOCKADDR*)&UDPServer,
+		&iUDPServer);
+
+	if (iReceiveFrom == SOCKET_ERROR) {
+		cout << "Blad odebrania wiadomosci o numerze " << WSAGetLastError() << endl;
+
+	}
+
+	//odebranie danych od klienta
+	cout << "Odebrano pomyslnie " << endl << endl;
+	cout << "Odebrana wiadomosc: " << BufferReceive << endl << endl;
+
+
+
+
+
+
+	//::_________________________________________________________________________________::
 	//zamkniecie gniazda
 	iCloseSocket = closesocket(UDPSocketClient);
 
@@ -101,6 +253,6 @@ int main()
 	system("PAUSE");
 	return 0;
 
-	//----------------
+	//::_________________________________________________________________________________::
 }
 //.server
--- Serwer/serwer.cpp
+++ Serwer/serwer.cpp
@@ -3,18 +3,49 @@
 #define UNICODE
 #endif
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
 
+constexpr auto PORT = 8000;
 
 #include <iostream>
 #include <stdio.h>
 #include <signal.h>
 #include <Windows.h>
 #include <winsock2.h>
+#include <ctime>
+#include <string>
 
 #pragma comment (lib, "Ws2_32.lib")
 
 using namespace std;
+
+int getId(bool id[]) {
+	for(int i=0; i<8; i++) {
+		if(id[i]) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+string get_godzina() {
+
+	time_t currentTime;
+	struct tm* localTime;
+
+	time(&currentTime);                   // Get the current time
+	localTime = localtime(&currentTime);  // Convert the current time to the local time
+
+	int hour = localTime->tm_hour;
+	int minute = localTime->tm_min;
+
+	string godzina = "";
+	godzina += to_string(hour) + ":" + to_string(minute);
+
+	return godzina;
+}
+
 
 int main()
 {
@@ -22,7 +53,6 @@ int main()
 
 
 	static char IP[] = "127.0.0.1"; //host IP
-	unsigned short PORT = 8000;
 	WORD versionRequested = MAKEWORD(2, 2); //wersja WinSock
 							
 	cout << "-----UDP SERWER----- " << endl;
@@ -34,15 +64,31 @@ int main()
 	SOCKET UDPSocketServer;
 	struct sockaddr_in UDPClient;
 
-	char Buffer[512]; //wiadomosc do przeslania 
-	int iBuffferLength = strlen(Buffer) +1; //dlugosc buffera 
+	char BufferReceive[800]; //wiadomosc doebrana 
+	int iBuffferReceiveLength = strlen(BufferReceive) +1; //dlugosc buffera do odebrania
+	
 
 	int iBind;
 	int iReceiveFrom;
-
+	string komunikat_otrzymany;
 	int iUDPClient = sizeof(UDPClient);
 	int iCloseSocket;
+
+	//do wys³ania zwrotnej wiaodmoœci
+	int iSendTo;
+	char BufferResponse[800]; //wiadomosc doebrana 
+	int iBuffferResponseLength; //dlugosc buffera do odebrania
+	string komunikat_zwrotny;
+
+	/*
+	bool id_client[8];
+	int liczba_client = 0; //max 8 polaczonych klientow
+
+	for(int i=0; i<8; i++) {
+		id_client[i] = true;
+	}
 	//--------------------------------
+	*/
 	//inicjalizacja WinSock
 
 	iWsaStartup = WSAStartup(versionRequested, &WinSockData);//wersja winsock
@@ -59,6 +105,7 @@ int main()
 	//--------------------------------
 	//stworzenie gniazda ---------------
 	UDPSocketServer = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); //protokó³ UDP
+
 	if (UDPSocketServer == INVALID_SOCKET) {
 		cout << "Blad w tworzeniu gniazda o numerze " << WSAGetLastError() << endl;
 	}
@@ -75,12 +122,20 @@ int main()
 	}
 	cout << "Laczenie z serwerem pomyslne " << endl;
 	cout << "Czekam ..." << endl;
-	//---------------------
-	
+	//--------------
+	//odebranie komunikatu o id, nastêpnie odes³anie numeru id do klienta
+	//odebranie komunikatu z proœb¹ o id
+
+
+
+	//oper#mnozenie@:stat#OKEY@iden#sesja0(numer w tablicy 0)#12:00#27@
+
+	strcpy(BufferReceive, komunikat_otrzymany.c_str());
+
 	iReceiveFrom = recvfrom(
 		UDPSocketServer,
-		Buffer,
-		iBuffferLength,
+		BufferReceive,
+		iBuffferReceiveLength,
 		MSG_PEEK,
 		(SOCKADDR*)&UDPClient,
 		&iUDPClient);
@@ -92,30 +147,59 @@ int main()
 
 	//odebranie danych od klienta
 	cout << "Odebrano pomyslnie " << endl << endl;
-	cout << "Odebrana wiadomosc: " << Buffer << endl << endl;
+	cout << "Odebrana wiadomosc: " << BufferReceive << endl << endl;
 
-	//------------------------
+
+	//serwer wysy³a wiadomosc do klienta
+	//---
+	komunikat_zwrotny = "Jestesmy zgubieni";
+	
+
+	strcpy(BufferResponse, komunikat_zwrotny.c_str());
+
+	iBuffferResponseLength = strlen(BufferResponse) + 1; //dlugosc buffera do wyslania
+
+	iSendTo = sendto(
+		UDPSocketServer,
+		BufferResponse,
+		iBuffferResponseLength,
+		MSG_DONTROUTE,
+		(SOCKADDR*)&UDPClient,
+		sizeof(UDPClient));
+	//---------------------------
+
+	if (iSendTo == SOCKET_ERROR) {
+		cout << "Blad wysylania wiadomosci o numerze " << WSAGetLastError() << endl;
+	}
+	cout << "Wiadomosc wyslana pomyslne " << endl;
+
 	//zamkniecie gniazda
-	iCloseSocket = closesocket(UDPSocketServer);
+	
+	//jesli klient wysle END zamkniecie serwera 
 
-	if (iCloseSocket == SOCKET_ERROR) {
-		cout << "Blad zamkniecia gniazda o numerze " << WSAGetLastError() << endl;
+	
+		iCloseSocket = closesocket(UDPSocketServer);
 
-	}
-	cout << "Gniazdo zamknieto pomyslnie " << endl;
+		if (iCloseSocket == SOCKET_ERROR) {
+			cout << "Blad zamkniecia gniazda o numerze " << WSAGetLastError() << endl;
 
-	//----------------
-	//zamkniecie WinSock
-	iWsaCleanup = WSACleanup();
+		}
+		cout << "Gniazdo zamknieto pomyslnie " << endl;
 
-	if (iWsaCleanup == SOCKET_ERROR) {
-		cout << "Blad zamkniecia WinSock o numerze " << WSAGetLastError() << endl;
+		//----------------
+		//zamkniecie WinSock
+		iWsaCleanup = WSACleanup();
 
-	}
-	cout << "WinSock zamknieto pomyslnie " << endl;
+		if (iWsaCleanup == SOCKET_ERROR) {
+			cout << "Blad zamkniecia WinSock o numerze " << WSAGetLastError() << endl;
 
-	system("PAUSE");
-	return 0;
+		}
+		cout << "WinSock zamknieto pomyslnie " << endl;
+
+
+		system("PAUSE");
+		return 0;
+	
 
 	//----------------
 }
